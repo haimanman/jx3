b12e92859600df89408bd2a62b107f254b0608b2
haimanman <haiman@czxiu.com>
2012-12-09 00:31:30 +0800

    调整主页加入工具

---
+++ update_version.php
@@ -0,0 +1,7 @@
+<?php
+// update gh-pages index.html
+$v = trim(file_get_contents("VERSION"));
+$b = file_get_contents("index.html");
+$b = preg_replace("#HM-.+?\.zip#", "HM-$v.zip", $b);
+$b = preg_replace("#date-->.+?\)#", "date-->" . date("Y/m/d") . ")", $b);
+file_put_contents("index.html", $b);
