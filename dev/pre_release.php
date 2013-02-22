edd47acd1626c0436693a9581f61fde71c2f1880
haimanman <haiman@czxiu.com>
2013-02-22 23:29:13 +0800

    年兽活动结束，暂时移除砸罐功能

--- dev/pre_release.php
+++ dev/pre_release.php
@@ -86,6 +86,9 @@ for ($i = 0; $i < count($required); $i++)
 }
 foreach ($files as $file)
 {
+	// remove HM_Taoguan.lua
+	if ($file === 'lab/HM_Taoguan.lua')
+		continue;
 	if (in_array(basename($file), $required)) continue;
 	$file = str_replace('/', '\\', $file);
 	$info .= "lua_{$i}=interface\\HM\\$file\r\n";
