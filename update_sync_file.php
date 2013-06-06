4724a909071e32411b2e2f369f36c8301484d60c
haimanman <haiman@czxiu.com>
2013-06-07 01:07:46 +0800

    update JX3HM-2.1.exe

--- update_sync_file.php
+++ update_sync_file.php
@@ -144,7 +144,7 @@ $content = <<<EOF
   <name><![CDATA[{$name}]]></name>
   <desc><![CDATA[{$desc}]]></desc>
   <version dword="$dword_ver">$version</version>
-  <clientVersion dword="0x10000">1.0beta</clientVersion>
+  <clientVersion dword="0x20100">2.1.0</clientVersion>
   <urlPrefix>http://jx3.hightman.cn/sync/HM/</urlPrefix>
   <info>
     $info
