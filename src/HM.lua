3dbde7eb7962983eb1272edbce6488f023517faf
haimanman <haiman@czxiu.com>
2013-06-22 21:12:13 +0800

    Internal alpha 2.1.3a2

--- src/HM.lua
+++ src/HM.lua
@@ -27,7 +27,7 @@ _L = _HM_GetLang()
 -- 本地函数和变量
 ---------------------------------------------------------------------
 local _HM = {
-	dwVersion = 0x2010301,
+	dwVersion = 0x2010302,
 	szBuildDate = "20130622",
 	szTitle = _L["HM, JX3 Plug-in Collection"],
 	szShort = _L["HM Plug"],
