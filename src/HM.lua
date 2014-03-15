ebf05bb1040d10ab4636d19cc55b31bc776ee234
haimanman <haiman@czxiu.com>
2014-03-15 16:32:08 +0800

    Release 2.2.13b2

--- src/HM.lua
+++ src/HM.lua
@@ -27,8 +27,8 @@ _L = _HM_GetLang()
 -- 本地函数和变量
 ---------------------------------------------------------------------
 local _HM = {
-	dwVersion = 0x2020d01,
-	szBuildDate = "20140302",
+	dwVersion = 0x2020d02,
+	szBuildDate = "20140315",
 	szTitle = _L["HM, JX3 Plug-in Collection"],
 	szShort = _L["HM Plug"],
 	szIniFile = "interface\\HM\\ui\\HM.ini",
