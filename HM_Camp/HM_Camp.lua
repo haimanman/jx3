0290af9a1e9563f267772447fd815249bbd36052
haimanman <haiman@czxiu.com>
2014-07-22 22:28:59 +0800

    优化攻防屏蔽在上线后重置，默认关闭自动合石头

--- HM_Camp/HM_Camp.lua
+++ HM_Camp/HM_Camp.lua
@@ -706,7 +706,8 @@ HM.RegisterEvent("PARTY_UPDATE_MEMBER_INFO", function()
 end)
 HM.RegisterEvent("SYNC_ROLE_DATA_END", function()
 	if HM_Camp.bHideEnable then
-		_HM_Camp.HideGF(true, true)
+		_HM_Camp.HideGF(false)
+		--_HM_Camp.HideGF(true, true)
 	end
 end)
 HM.RegisterEvent("ON_CAN_ENTER_MAP_NOTIFY", function()
