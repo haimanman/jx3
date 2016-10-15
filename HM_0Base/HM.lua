f9db29587034bdf4b96751121b5a8586b0b85f20
haimanman <haiman@czxiu.com>
2016-10-15 23:22:18 +0800

    Release 2.4.48

--- HM_0Base/HM.lua
+++ HM_0Base/HM.lua
@@ -33,8 +33,8 @@ _L = _HM_GetLang()
 -- 本地函数和变量
 ---------------------------------------------------------------------
 local _HM = {
-	dwVersion = 0x2042f00,
-	szBuildDate = "20160815",
+	dwVersion = 0x2043000,
+	szBuildDate = "20161015",
 	szTitle = _L["HM, JX3 Plug-in Collection"],
 	szShort = _L["HM Plug"],
 	szIniFile = _L["@hm_ini@"],
